80b2cc13254170d9bd23a00ab0f48e86ae755ec8
Keshav Gupta <keshav1032@gmail.com>
2018-03-22 00:26:17 -0400

    union find rank and path compression

--- src/union_find/UnionFind.java
+++ src/union_find/UnionFind.java
@@ -7,11 +7,13 @@ public class UnionFind {
     int num_verts;
     int[][] edges;
     int[] parent;
+    int[] rank;
 
     UnionFind(int num_verts, int[][] edges) {
         this.edges = edges;
         this.num_verts = num_verts;
         this.parent = new int[num_verts];
+        this.rank = new int[num_verts];
         for (int i = 0; i < num_verts; i++) {
             parent[i] = i;
         }
@@ -20,15 +22,28 @@ public class UnionFind {
     public void union(int u, int v) {
         int uPar = find(u);
         int vPar = find(v);
-        parent[uPar] = vPar;
+        int uRank = rank[uPar];
+        int vRank = rank[vPar];
+        if(uRank > vRank) {
+            parent[vPar] = uPar;
+        }
+        else if(uRank < vRank) {
+            parent[uPar] = vPar;
+        }
+        else {
+            parent[uPar] = vPar;
+            rank[vPar]++;
+        }
+
     }
 
     public int find(int x) {
         if(parent[x] == x) {
             return x;
         }
-        
-        return find(parent[x]);
+        int r = find(parent[x]); // path compression
+        parent[x] = r;
+        return r;
     }
     
     public boolean isCyclePresent() {
@@ -52,9 +67,9 @@ public class UnionFind {
         });
 
         System.out.println(uf.isCyclePresent());
-        uf = new UnionFind(4,
+        uf = new UnionFind(7,
                 new int[][] {
-                        {0, 1}, {1, 2}, {1, 0}
+                        {0, 1}, {1, 2}, {2, 3}, {3,4}, {4, 5}, {5, 2}
                 });
 
         System.out.println(uf.isCyclePresent());
