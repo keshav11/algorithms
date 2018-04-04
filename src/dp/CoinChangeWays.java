28c7ce9cad29fd6b28baa09c7dd71fa345763c08
Keshav Gupta <keshav1032@gmail.com>
2018-04-04 14:07:25 -0400

    coin change num ways

---
+++ src/dp/CoinChangeWays.java
@@ -0,0 +1,30 @@
+package dp;
+
+
+public class CoinChangeWays {
+
+    int numWays(int[] coins, int total) {
+        int[][] dp = new int[total+1][coins.length];
+        for (int i = 0; i <= total; i++) {
+            for(int j = 0; j < coins.length; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                }
+                else if(i >= coins[j]) {
+
+                    dp[i][j] = dp[i-coins[j]][j] + dp[i][j-1];
+                }
+                else {
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[total][coins.length-1];
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CoinChangeWays().numWays(new int[] {2, 3, 7}, 23 ));
+    }
+}
