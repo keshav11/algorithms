8d87a4ef464a1ba29182c0e4ab9fd26d9681f085
Keshav Gupta <keshav1032@gmail.com>
2018-04-05 23:04:37 -0400

    added 2 dp problems

---
+++ src/dp/MaxSumSubarray.java
@@ -0,0 +1,34 @@
+package dp;
+
+public class MaxSumSubarray {
+    int maxSubarray(int arr[]) {
+
+        int maxSum = arr[0];
+        int currSum = arr[0];
+        int maxLeft = 0;
+        int maxRight = 0;
+        int currLeft = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if(currSum < 0) {
+                currSum = 0;
+                currLeft = i;
+            }
+            currSum += arr[i];
+            if(currSum > maxSum) {
+                maxSum = currSum;
+                maxLeft = currLeft;
+                maxRight = i;
+            }
+        }
+
+        System.out.println(maxLeft + " " + maxRight + " " + maxSum);
+
+        return maxSum;
+
+    }
+
+    public static void main(String[] args) {
+        new MaxSumSubarray().maxSubarray(new int[] {-1, 1, 3, 4, -5, 1, 2, -9});
+    }
+}
---
+++ src/dp/SubsetSum.java
@@ -0,0 +1,23 @@
+package dp;
+
+public class SubsetSum {
+
+    public static boolean isSubsetSum(int[] set, int num) {
+        boolean[] dp = new boolean[num+1];
+        dp[0] = true; // 0 sum is always possible
+        for(int j = 0; j < set.length; j++) {
+            for(int i = num; i >= set[j]; i--) {
+                if(i >= set[j])
+                    dp[i] = dp[i] || dp[i-set[j]];
+            }
+        }
+        return dp[num];
+    }
+
+    public static void main(String[] args){
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(isSubsetSum(new int[] {1, 2}, i));
+        }
+    }
+
+}
